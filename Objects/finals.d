30b4a0c69f5bb9f0bdaff27bd7d3a54936b99703
bui quoc viet <buiquocviet3005@gmail.com>
2025-11-14 13:16:57 +0700

    add the rest

---
+++ Objects/finals.d
@@ -0,0 +1,9 @@
+.\objects\finals.o: finals.c
+.\objects\finals.o: finals.h
+.\objects\finals.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\finals.o: .\RTE\Device\MKL46Z256VLL4\MKL46Z4.h
+.\objects\finals.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\core_cm0plus.h
+.\objects\finals.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\finals.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\finals.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\finals.o: .\RTE\Device\MKL46Z256VLL4\system_MKL46Z4.h
